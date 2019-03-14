cc44d0e04c4e25ee97d2ff3e2eaeb82d85bb2e5f
Chris Varughese <cjamiev@icloud.com>
2019-03-13 23:12:41 -0500

    Removing switch statement

--- reactjs/src/sandbox/reducers/todos.js
+++ reactjs/src/sandbox/reducers/todos.js
@@ -1,19 +1,18 @@
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
-      ];
-    case 'TOGGLE_TODO':
-      return state.map(todo => (todo.id === action.id ? { ...todo, completed: !todo.completed } : todo));
-    default:
-      return state;
-  }
+const initialState = [];
+
+const todos = (state = initialState, action) => {
+  const todoCases = {
+    'ADD_TODO': () => {
+      return [...state, { id: action.id, text: action.text, completed: false }];
+    },
+    'TOGGLE_TODO': () => {
+      return state.map(todo => {
+        return (todo.id === action.id ? { ...todo, completed: !todo.completed } : todo);
+      });
+    }
+  };
+
+  return todoCases.hasOwnProperty(action.type) ? todoCases[action.type]() : state;
 };
 
 export default todos;
--- reactjs/src/sandbox/reducers/visibilityFilter.js
+++ reactjs/src/sandbox/reducers/visibilityFilter.js
@@ -1,12 +1,13 @@
 import { VisibilityFilters } from '../actions';
 
 const visibilityFilter = (state = VisibilityFilters.SHOW_ALL, action) => {
-  switch (action.type) {
-  case 'SET_VISIBILITY_FILTER':
-    return action.filter;
-  default:
-    return state;
-  }
+  const visibilityFilterCases = {
+    'SET_VISIBILITY_FILTER': () => {
+      return action.filter;
+    }
+  };
+
+  return visibilityFilterCases.hasOwnProperty(action.type) ? visibilityFilterCases[action.type]() : state;
 };
 
 export default visibilityFilter;
