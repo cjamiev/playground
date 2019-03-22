c718ff6dd1e1fb6a0e2b39938f8637ae9d7751fd
Chris Varughese <cjamiev@icloud.com>
2019-03-21 21:12:08 -0500

    Removing service worker and api utility functions

--- reactjs/src/utility.js
+++ reactjs/src/utility.js
@@ -1,8 +1,3 @@
-const HEADERS = new Headers({
-  'Content-Type': 'application/json',
-  Accept: 'application/json'
-});
-
 export const swapArrayElementPositions = (oldArray, indexA, indexB) => {
   if (indexA < 0 || indexB < 0 || indexA >= oldArray.length || indexB >= oldArray.length) {
     return oldArray;
@@ -22,25 +17,4 @@ export const decrementElementIndex = (originalArray, index) => {
 
 export const incrementElementIndex = (originalArray, index) => {
   return swapArrayElementPositions(originalArray, index, index + 1);
-};
-
-export const apiPost = (url, body) => {
-  return fetch(url, {
-    method: 'POST',
-    headers: HEADERS,
-    body: JSON.stringify(body)
-  })
-    .then(response => response.json())
-    .then(result => result)
-    .catch(error => console.log('error:', error));
-};
-
-export const apiGet = url => {
-  return fetch(url, {
-    method: 'GET',
-    headers: HEADERS
-  })
-    .then(response => response.json())
-    .then(result => result)
-    .catch(error => console.log('error:', error));
 };
