5c0e928cafd8fcf945be279755f8e1a66e85dbe1
Chris Varughese <cjamiev@icloud.com>
2019-07-06 17:48:41 -0500

    Added router with method chaining

--- server/index.js
+++ server/index.js
@@ -1,35 +1,62 @@
 const routerOne = (req, res) => {
-        if(req.url === '/test'){
-            console.log('get1');
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'testing get'}));        
-        }
-        if(req.url === '/test2'){
-            console.log('get2');
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'testing get2'}));        
-        }
+    if(req.url === '/test'){
+        console.log('get1');
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'testing get'}));        
+    }
+    if(req.url === '/test2'){
+        console.log('get2');
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'testing get2'}));        
+    }
 };
 
 const routerTwo = (req, res) => {
-        if(req.url === '/test'){
-            console.log('post1');
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'testing post'}));        
-        }
-        if(req.url === '/test2'){
-            console.log('post2');
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'testing post2'}));        
-        } 
+    if(req.url === '/test'){
+        console.log('post1');
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'testing post'}));        
+    }
+    if(req.url === '/test2'){
+        console.log('post2');
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'testing post2'}));        
+    } 
 };
 
-const route = (routesGET, routesPOST) => (request, response) => {
-    if(request.method === 'GET'){
-      routesGET(request, response);
-    } else {
-      routesPOST(request, response);
-    }
+const router = () => {
+    const routesWithMethodGet = [];
+    const routesWithMethodPost = [];
+    const routes = {
+      get(route) {
+        routesWithMethodGet.push(route);
+  
+        return this;
+      },
+      post(route) {
+        routesWithMethodPost.push(route);
+  
+        return this;
+      },
+      entries() {
+        return { routesWithMethodGet, routesWithMethodPost };
+      }
+    };
+  
+    return routes;
+};
+
+const route = (router) => {
+    return (request, response) => {
+        const { routesWithMethodGet, routesWithMethodPost } = router.entries();
+        console.log(routesWithMethodGet, routesWithMethodPost);
+    
+        if(request.method === 'GET'){
+          routesWithMethodGet.forEach(route => route(request, response));
+        } else {
+          routesWithMethodPost.forEach(route => route(request, response));
+        }
+    };
 };
 
 const testResponse = (request, response) => {
@@ -39,7 +66,10 @@ const testResponse = (request, response) => {
 	response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
 	response.setHeader('Access-Control-Allow-Headers', '*');
 
-    route(routerOne, routerTwo)(request, response);
+    const routes = router().get(routerOne).post(routerTwo);
+    console.log(routes);
+
+    route(routes)(request, response);
 };
 
 const http = require('http');
