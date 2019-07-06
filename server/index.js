6b701fd8153c7d9284cab78f3ca982a4aa8a9f6c
Chris Varughese <cjamiev@icloud.com>
2019-07-06 13:00:49 -0500

    Added seperate handlers for get and post routes

--- server/index.js
+++ server/index.js
@@ -1,47 +1,48 @@
-var http = require('http');
-
-const router = {
-    get: (req, res) => {
+const routerOne = (req, res) => {
         if(req.url === '/test'){
+            console.log('get1');
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({ message: 'testing get'}));        
-        } else {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'testing get other'}));
         }
+        if(req.url === '/test2'){
+            console.log('get2');
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'testing get2'}));        
+        }
+};
 
-    },
-    post: (req, res) => {
+const routerTwo = (req, res) => {
         if(req.url === '/test'){
+            console.log('post1');
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({ message: 'testing post'}));        
-        } else {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'testing get post'}));
         }
+        if(req.url === '/test2'){
+            console.log('post2');
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'testing post2'}));        
+        } 
+};
+
+const route = (routesGET, routesPOST) => (request, response) => {
+    if(request.method === 'GET'){
+      routesGET(request, response);
+    } else {
+      routesPOST(request, response);
     }
 };
 
-const route = (request, response) => {
+const testResponse = (request, response) => {
+    console.log('request ', request.url, request.method);
     response.setHeader('Access-Control-Allow-Origin', '*');
 	response.setHeader('Access-Control-Request-Method', '*');
 	response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
 	response.setHeader('Access-Control-Allow-Headers', '*');
 
-    if(request.method === 'GET'){
-      router.get(request, response);
-    } else {
-      router.post(request, response);
-    }
+    route(routerOne, routerTwo)(request, response);
 };
 
-const testResponse = (request, response) => {
-    console.log('request ', request.url);
+const http = require('http');
+http.createServer(testResponse).listen(8081);
 
-    route(request, response);
-};
-
-const defaultResponse = require('./defaultResponse').defaultResponse;
-
-http.createServer(testResponse).listen(8080);
-console.log('Server running at http://127.0.0.1:8080/');
+console.log('Server running at http://127.0.0.1:8081/');
