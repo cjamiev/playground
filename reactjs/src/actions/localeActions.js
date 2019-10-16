1cb400fb75794775bbb8e01a4161805c0974eb79
Chris Varughese <cjamiev@icloud.com>
2019-10-16 00:35:13 -0500

    Removing reactjs as it is no longer used

--- reactjs/src/actions/localeActions.js
+++
@@ -1,8 +0,0 @@
-export const CHANGE_LOCALE = 'app/LanguageToggle/CHANGE_LOCALE';
-
-export function changeLocale(languageLocale) {
-  return {
-    type: CHANGE_LOCALE,
-    locale: languageLocale
-  };
-}
--- reactjs/src/constants/regex.js
+++
@@ -1,2 +0,0 @@
-export const INTEGER_REGEX = /^\d+$/;
-export const DOLLAR_REGEX = /^\d+(\.\d{1,2})*$/;
