d86a5f5abcb4e796a78d7a0582c6e0b04d1cf540
Chris Varughese <cjamiev@icloud.com>
2020-12-20 16:06:39 -0600

    Replacing NavLink with useHistory

--- react-experiment/src/app/Navigation.js
+++ react-experiment/src/app/Navigation.js
@@ -1,37 +1,51 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { Fragment, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const navStyle = {
+  cursor: 'pointer'
+};
+
+const NAV_ITEMS = [
+  { label: 'Home', url: '/home'},
+  { label: 'Test Swap Select', url: '/test-swap-select'},
+  { label: 'Todo', url: '/todo-app'}
+];
 
 const Navigation = React.memo(() => {
+  const history = useHistory();
   const [toggleNav, setToggleNav] = useState(false);
+  const [currentUrl, setCurrentUrl] = useState(history.location.pathname);
   const expandNav = toggleNav ? 'navbar-collapse collapse show' : 'navbar-collapse collapse';
 
+  const renderNavItems = NAV_ITEMS.map(item => {
+    const navItemClass = currentUrl === item.url ? 'nav-item nav-link active': 'nav-item nav-link';
+    const handleClick = () => {
+      if(history.location.pathname !== item.url){
+        history.push(item.url);
+        setCurrentUrl(item.url);
+      }
+    };
+
+    return (
+      <div key={item.url} style={navStyle} className={navItemClass} onClick={handleClick}>
+        {item.label}
+      </div>
+    );
+  });
+
   return (
-    <>
+    <Fragment>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <button className="navbar-toggler" onClick={() => {setToggleNav(!toggleNav);}} aria-controls="navbarSupportedContent" aria-expanded={toggleNav} aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className={expandNav} id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/home">
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/test-swap-select">
-                Test Swap Select
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/todo-app">
-              Todo App
-              </NavLink>
-            </li>
+            {renderNavItems}
           </ul>
         </div>
       </nav>
-    </>
+    </Fragment>
   );
 });
 
