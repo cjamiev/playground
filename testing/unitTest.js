74708cd2aa349e3d3737098723bd6e3908d953ba
Chris Varughese <cjamiev@icloud.com>
2021-06-24 16:20:32 -0500

    Updating personal unit test to work again.

--- testing/unitTest.js
+++ testing/unitTest.js
@@ -1,4 +1,32 @@
-const { isEqual } = require('../src/util');
+const isEmpty = targetObject => !isNotEmpty(targetObject);
+const xOr = (a, b) => (!a && b) || (a && !b);
+const isObjectLike = value => value !== null && typeof value === 'object';
+const isEqual = (entry1, entry2) => {
+  if (!(isObjectLike(entry1) && isObjectLike(entry1))) {
+    return entry1 === entry2;
+  }
+  if (xOr(isEmpty(entry1), isEmpty(entry2))) {
+    return false;
+  }
+
+  const keys1 = Object.keys(entry1);
+  const keys2 = Object.keys(entry2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  const checkEquality = keys1.reduce((accumulator, key) => {
+    const child1 = entry1[key];
+    const child2 = entry2[key];
+
+    return isObjectLike(child1) && isObjectLike(child2)
+      ? accumulator && isEqual(child1, child2)
+      : accumulator && child1 === child2;
+  }, true);
+
+  return checkEquality;
+};
 
 const getFormatedMessage = (testMessage, args, expectedResult, receivedResult) => {
   const failMessage = `\nfail: ${testMessage} \n`;
