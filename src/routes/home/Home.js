4ad47daee9b3e51906c07090c0329cb1eb72a9e9
Chris Varughese <cjamiev@icloud.com>
2021-08-20 16:23:35 -0500

    Creating separate HomeFooter component

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -8,10 +8,10 @@ import Text from 'components/form/Text';
 import TextArea from 'components/form/TextArea';
 import Dropdown from 'components/form/Dropdown';
 import Switch from 'components/switch';
-import { DisplayTimer } from 'components/list/List';
 import { copyToClipboard } from 'helper/copy';
 import { sortByDelimiter, sortDescendingByDelimiter } from 'sort';
 import { isJSONString } from 'type-check';
+import HomeFooter from './HomeFooter';
 import './home.css';
 
 const ZERO = 0;
@@ -29,18 +29,6 @@ const Home = () => {
   const [delimiters, setDelimiters] = useState(DELIMITER_TYPES);
   const [find, setFind] = useState('');
   const [replace, setReplace] = useState('');
-  const [toggleClip, setToggleClip] = useState(false);
-  const [toggleTimer, setToggleTimer] = useState(false);
-  const [copyName, setCopyName] = useState('');
-  const [copyContent, setCopyContent] = useState('');
-  const [month, setMonth] = useState(today.getMonth()+ONE);
-  const [day, setDay] = useState(today.getDate());
-  const [year, setYear] = useState(today.getFullYear());
-  const [hour, setHour] = useState((today.getHours()+ONE)%12);
-  const [minute, setMinute] = useState(ZERO);
-  const [second, setSecond] = useState(ZERO);
-  const [amOrPmMode, setAmOrPmMode] = useState(today.getHours()+ ONE - 12 > ZERO ? ONE : ZERO);
-  const [removeMode, setRemoveMode] = useState(false);
   const dispatch = useDispatch();
   const { directory, file, error, result } = useSelector(state => state.home);
 
@@ -66,85 +54,6 @@ const Home = () => {
   });
 
   const selectedDelimiter = delimiters.find(item => item.selected);
-  const footerClass = toggleClip ? 'quick-clipboard-add-new-container quick-clipboard-add-new-container-active' : 'quick-clipboard-add-new-container';
-
-  const clipboard = localStorage.getItem('clipboard') || '[]';
-  const clipboardData = JSON.parse(clipboard);
-  const copyButtons = clipboardData.filter(item => item.type === 'copy').map(item => {
-    return <Button key={item.name} label={item.name} onClick={() => {
-      if(!removeMode) {
-        copyToClipboard(item.value);
-      } else {
-        const clips = JSON.parse(localStorage.getItem('clipboard') || '[]');
-        const filtered =clips.filter(clip => clip.name !== item.name);
-        localStorage.setItem('clipboard', JSON.stringify(filtered));
-      }
-    }} />;
-  });
-  const timers = clipboardData.filter(item => item.type === 'timer').map(item => {
-    const newTimerDate = new Date(item.value.year,item.value.month-ONE,item.value.day,item.value.hour,item.value.minute,item.value.second);
-    return (
-      <div key={item.name} onClick={() => {
-        if(removeMode) {
-          const clips = JSON.parse(localStorage.getItem('clipboard') || '[]');
-          const filtered =clips.filter(clip => clip.name !== item.name);
-          localStorage.setItem('clipboard', JSON.stringify(filtered));
-        }
-      }}>
-        <DisplayTimer label={item.name} date={newTimerDate} />
-      </div>
-    );
-  });
-
-  const footerComponent = () => {
-    return (
-      <div>
-        <div id="quick-clipboard-add-new-container" className='quick-clipboard-add-new-container quick-clipboard-add-new-container-active'>
-          {toggleClip && <div className="quick-clipboard-add-form" >
-            <Text label='Name' selected={copyName} onChange={({selected}) => { setCopyName(selected); }} />
-            <TextArea label="Value" selected={copyContent} onChange={({ selected }) => { setCopyContent(selected); }}/>
-            <button className="quick-clipboard-submit-btn quick-clipboard-add-btn" onClick={
-              () => {
-                const clip = JSON.parse(localStorage.getItem('clipboard') || '[]');
-                clip.push({ name: copyName, value: copyContent, type: 'copy' });
-                localStorage.setItem('clipboard', JSON.stringify(clip));
-              }
-            }>Submit</button>
-          </div>}
-          {toggleTimer && <div className="quick-clipboard-add-form">
-            <div>
-              <Text label='Name' selected={copyName} onChange={({selected}) => { setCopyName(selected); }} />
-            </div>
-            <div id="timer-fields-date">
-              <Text label='Month' selected={month} onChange={({selected}) => { setMonth(selected); }} />
-              <Text label='Day' selected={day} onChange={({selected}) => { setDay(selected); }} />
-              <Text label='Year' selected={year} onChange={({selected}) => { setYear(selected); }} />
-            </div>
-            <div id="timer-fields-clock">
-              <Text label='Hour' selected={hour} onChange={({selected}) => { setHour(selected); }} />
-              <Text label='Minute' selected={minute} onChange={({selected}) => { setMinute(selected); }} />
-              <Text label='Second' selected={second} onChange={({selected}) => { setSecond(selected); }} />
-              <Switch data={[{ label: 'am' }, { label: 'pm'}]} switchIndex={amOrPmMode} onToggleSwitch={(index) => { setAmOrPmMode(index);}} />
-            </div>
-            <button className="quick-clipboard-submit-btn quick-clipboard-add-btn" onClick={
-              () => {
-                const parsedHour = amOrPmMode ? Number(hour) + 12 : Number(hour);
-                const timerContent = { month: Number(month), day: Number(day), year: Number(year), hour: parsedHour, minute: Number(minute), second: Number(second) };
-                const clip = JSON.parse(localStorage.getItem('clipboard') || '[]');
-                clip.push({ name: copyName, value: timerContent, type: 'timer' });
-                localStorage.setItem('clipboard', JSON.stringify(clip));
-              }
-            }>Submit</button>
-          </div>}
-        </div>
-        <Button label='Add Clip' onClick={() => { setToggleClip(!toggleClip); setToggleTimer(false);}} />
-        <Button label='Add Timer' onClick={() => { setToggleTimer(!toggleTimer); setToggleClip(false);}} />
-        <Button label='Remove' onClick={() => { setRemoveMode(!removeMode); }} />
-        {copyButtons}
-        {timers}
-      </div>
-    );
-  };
 
   return (
     <Page
@@ -153,7 +62,7 @@ const Home = () => {
           {directoryFilesName}
         </div>
       }
-      footerComponent={footerComponent()}
+      footerComponent={HomeFooter()}
     >
       <div className="home__btns">
         <Text placeholder='Enter File Name' selected={name} onChange={({selected}) => { setName(selected); }} />
---
+++ src/routes/home/HomeFooter.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import Button from 'components/button';
+import Text from 'components/form/Text';
+import TextArea from 'components/form/TextArea';
+import Dropdown from 'components/form/Dropdown';
+import Switch from 'components/switch';
+import { DisplayTimer } from 'components/list/List';
+import { copyToClipboard } from 'helper/copy';
+
+const ZERO = 0;
+const ONE = 1;
+const TWELVE = 12;
+
+const HomeFooter = () => {
+  const today = new Date();
+  const [toggleClip, setToggleClip] = useState(false);
+  const [toggleTimer, setToggleTimer] = useState(false);
+  const [copyName, setCopyName] = useState('');
+  const [copyContent, setCopyContent] = useState('');
+  const [month, setMonth] = useState(today.getMonth()+ONE);
+  const [day, setDay] = useState(today.getDate());
+  const [year, setYear] = useState(today.getFullYear());
+  const [hour, setHour] = useState((today.getHours()+ONE)%TWELVE);
+  const [minute, setMinute] = useState(ZERO);
+  const [second, setSecond] = useState(ZERO);
+  const [amOrPmMode, setAmOrPmMode] = useState(today.getHours()+ ONE - TWELVE > ZERO ? ONE : ZERO);
+  const [removeMode, setRemoveMode] = useState(false);
+
+  const footerClass = toggleClip ? 'quick-clipboard-add-new-container quick-clipboard-add-new-container-active' : 'quick-clipboard-add-new-container';
+
+  const clipboard = localStorage.getItem('clipboard') || '[]';
+  const clipboardData = JSON.parse(clipboard);
+  const copyButtons = clipboardData.filter(item => item.type === 'copy').map(item => {
+    return <Button key={item.name} label={item.name} onClick={() => {
+      if(!removeMode) {
+        copyToClipboard(item.value);
+      } else {
+        const clips = JSON.parse(localStorage.getItem('clipboard') || '[]');
+        const filtered =clips.filter(clip => clip.name !== item.name);
+        localStorage.setItem('clipboard', JSON.stringify(filtered));
+        setRemoveMode(false);
+      }
+    }} />;
+  });
+  const timers = clipboardData.filter(item => item.type === 'timer').map(item => {
+    const newTimerDate = new Date(item.value.year,item.value.month-ONE,item.value.day,item.value.hour,item.value.minute,item.value.second);
+    return (
+      <div key={item.name} onClick={() => {
+        if(removeMode) {
+          const clips = JSON.parse(localStorage.getItem('clipboard') || '[]');
+          const filtered =clips.filter(clip => clip.name !== item.name);
+          localStorage.setItem('clipboard', JSON.stringify(filtered));
+          setRemoveMode(false);
+        }
+      }}>
+        <DisplayTimer label={item.name} date={newTimerDate} />
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div id="quick-clipboard-add-new-container" className='quick-clipboard-add-new-container quick-clipboard-add-new-container-active'>
+        {toggleClip && <div className="quick-clipboard-add-form" >
+          <Text label='Name' selected={copyName} onChange={({selected}) => { setCopyName(selected); }} />
+          <TextArea label="Value" selected={copyContent} onChange={({ selected }) => { setCopyContent(selected); }}/>
+          <button className="quick-clipboard-submit-btn quick-clipboard-add-btn" onClick={
+            () => {
+              const clip = JSON.parse(localStorage.getItem('clipboard') || '[]');
+              clip.push({ name: copyName, value: copyContent, type: 'copy' });
+              localStorage.setItem('clipboard', JSON.stringify(clip));
+              setToggleClip(false);
+            }
+          }>Submit</button>
+        </div>}
+        {toggleTimer && <div className="quick-clipboard-add-form">
+          <div>
+            <Text label='Name' selected={copyName} onChange={({selected}) => { setCopyName(selected); }} />
+          </div>
+          <div id="timer-fields-date">
+            <Text label='Month' selected={month} onChange={({selected}) => { setMonth(selected); }} />
+            <Text label='Day' selected={day} onChange={({selected}) => { setDay(selected); }} />
+            <Text label='Year' selected={year} onChange={({selected}) => { setYear(selected); }} />
+          </div>
+          <div id="timer-fields-clock">
+            <Text label='Hour' selected={hour} onChange={({selected}) => { setHour(selected); }} />
+            <Text label='Minute' selected={minute} onChange={({selected}) => { setMinute(selected); }} />
+            <Text label='Second' selected={second} onChange={({selected}) => { setSecond(selected); }} />
+            <Switch data={[{ label: 'am' }, { label: 'pm'}]} switchIndex={amOrPmMode} onToggleSwitch={(index) => { setAmOrPmMode(index);}} />
+          </div>
+          <button className="quick-clipboard-submit-btn quick-clipboard-add-btn" onClick={
+            () => {
+              const parsedHour = amOrPmMode ? Number(hour) + TWELVE : Number(hour);
+              const timerContent = { month: Number(month), day: Number(day), year: Number(year), hour: parsedHour, minute: Number(minute), second: Number(second) };
+              const clip = JSON.parse(localStorage.getItem('clipboard') || '[]');
+              clip.push({ name: copyName, value: timerContent, type: 'timer' });
+              localStorage.setItem('clipboard', JSON.stringify(clip));
+              setToggleTimer(false);
+            }
+          }>Submit</button>
+        </div>}
+      </div>
+      <Button label='Add Clip' classColor='primary' onClick={() => { setToggleClip(!toggleClip); setToggleTimer(false);}} />
+      <Button label='Add Timer' classColor='primary' onClick={() => { setToggleTimer(!toggleTimer); setToggleClip(false);}} />
+      <Button label='Remove' classColor='secondary' onClick={() => { setRemoveMode(!removeMode); }} />
+      {copyButtons}
+      {timers}
+    </div>
+  );
+};
+
+export default HomeFooter;
