f369a312805d03b7c8848b6f5b98fcecc559b4e3
Chris Varughese <cjamiev@icloud.com>
2021-10-03 17:34:05 -0500

    Reverting q.js

--- q.js
+++ q.js
@@ -1,13 +1,1 @@
-const data = {
-  number: 126,
-  string: 'testing',
-  boolean: true,
-  list: [1, 2, 3],
-  object: {
-    number: 126,
-    string: 'testing',
-    boolean: true,
-    list: [1, 2, 3]
-  }
-};
 console.log('Hello, World');
