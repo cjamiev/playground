4b5582ae04ffe125cffde1dd0e42c5246de09be1
Chris Varughese <cjamiev@icloud.com>
2021-10-06 17:56:28 -0500

    Updating testing script Updating Icons to trim decimals

--- src/components/icons/InfoIcon.js
+++ src/components/icons/InfoIcon.js
@@ -3,18 +3,18 @@ import React from 'react';
 const InfoIcon = () => {
   return (
     <g aria-label="info">
-      <ellipse className="icon--hollow" cx="26.39213" cy="26.524538" rx="24.556198" ry="24.88722" />
-      <g transform="matrix(0.85469773,0,0,0.85469773,3.8444566,3.6546467)">
+      <ellipse className="icon--hollow" cx="26.39" cy="26.52" rx="24.55" ry="24.88" />
+      <g transform="matrix(0.85,0,0,0.85,3.84,3.65)">
         <rect
           className="icon--black"
-          width="10.406781"
-          height="34.505215"
-          x="21.254942"
-          y="16.776503"
-          rx="53.485786"
-          ry="7.4267721"
+          width="10.40"
+          height="34.50"
+          x="21.25"
+          y="16.77"
+          rx="53.48"
+          ry="7.42"
         />
-        <ellipse className="icon--black" cx="26.458334" cy="6.0908132" rx="4.6343145" ry="4.1046786" />
+        <ellipse className="icon--black" cx="26.45" cy="6.09" rx="4.63" ry="4.10" />
       </g>
     </g>
   );
