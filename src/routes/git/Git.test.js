7b58b78b4c1fabbc64a87a455e529ecba63378e5
Chris Varughese <cjamiev@icloud.com>
2021-10-09 18:46:40 -0500

    Updating git tests

--- src/routes/git/Git.test.js
+++ src/routes/git/Git.test.js
@@ -1,17 +1,96 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { reduxTestWrapper } from 'testHelper';
+import { reduxTestWrapper, mockApi } from 'testHelper';
 import Git from './Git';
+import api from 'api';
 
+const mockGet = (url) => {
+  return Promise.resolve({
+    data: {
+      message: 'test message'
+    }
+  });
+};
+const mockPost = () => {return Promise.resolve({});};
+const apiMock = mockApi(mockGet, mockPost);
+
+const ZERO = 0;
+const ONE = 1;
+const rootDir = './';
+const name = 'test-stash';
 const defaultStoreProps = {
   git: {
-    data: 'test123'
+    remoteUrl: 'test-url',
+    branches: ['branch1', 'branch2'],
+    stashes: ['stash{1}', 'stash{2}'],
+    message: ''
   }
 };
 
 describe('Git', () => {
-  it('should render', () => {
+  it('should handle remote url', () => {
+    document.execCommand = jest.fn();
+    const spy = jest.spyOn(document.body, 'appendChild');
     reduxTestWrapper(Git, {}, defaultStoreProps);
 
-    expect(screen.getByText('git')).toBeInTheDocument();
+    const copyBtn = screen.getByLabelText('copy');
+    fireEvent.click(copyBtn);
+    const copyEl = spy.mock.calls[ONE][ZERO];
+
+    expect(screen.getByText(`Remote Url: ${defaultStoreProps.git.remoteUrl}`)).toBeInTheDocument();
+    expect(copyEl.value).toEqual(defaultStoreProps.git.remoteUrl);
+  });
+
+  it('handle create stash', () => {
+    reduxTestWrapper(Git, {}, defaultStoreProps);
+
+    const nameField = screen.getByLabelText('Name text field');
+
+    fireEvent.change(nameField, { target: { value: name } });
+    const createStashBtn = screen.getByText(`Create Stash ${name}`);
+    fireEvent.click(createStashBtn);
+
+    expect(api.get).toHaveBeenCalledWith(`/git/?type=stash&root=${rootDir}&name=${name}`);
+  });
+
+  it('switch branch', () => {
+    reduxTestWrapper(Git, {}, defaultStoreProps);
+
+    const branchesBtn = screen.getByText('Branches');
+    fireEvent.click(branchesBtn);
+    fireEvent.click(screen.getByText('branch1'));
+    fireEvent.click(screen.getByText('Checkout branch1'));
+
+    expect(api.get).toHaveBeenCalledWith(`/git/?type=select&root=${rootDir}&name=branch1`);
+  });
+
+  it('delete branch', () => {
+    reduxTestWrapper(Git, {}, defaultStoreProps);
+
+    const branchesBtn = screen.getByText('Branches');
+    fireEvent.click(branchesBtn);
+    fireEvent.click(screen.getByText('branch1'));
+    fireEvent.click(screen.getByText('Delete branch1'));
+
+    expect(api.get).toHaveBeenCalledWith(`/git/?type=delete&root=${rootDir}&name=branch1`);
+  });
+
+  it('select stash', () => {
+    reduxTestWrapper(Git, {}, defaultStoreProps);
+
+    const stashesBtn = screen.getByText('Stashes');
+    fireEvent.click(stashesBtn);
+    fireEvent.click(screen.getByText('stash{1}'));
+    fireEvent.click(screen.getByText('Switch Stash 1'));
+
+    expect(api.get).toHaveBeenCalledWith(`/git/?type=selectstash&root=${rootDir}&name=1`);
+  });
+
+  it('handle reset', () => {
+    reduxTestWrapper(Git, {}, defaultStoreProps);
+
+    const resetBtn = screen.getByText('Reset');
+    fireEvent.click(resetBtn);
+
+    expect(api.get).toHaveBeenCalledWith(`/git/?type=reset&root=${rootDir}`);
   });
 });
