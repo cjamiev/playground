6c2ea3551a18d176cec9a81d3d8d9e31372bd3bf
Chris Varughese <cjamiev@icloud.com>
2021-11-05 23:49:08 -0500

    Updating switch css

--- src/components/switch/Switch.js
+++ src/components/switch/Switch.js
@@ -3,12 +3,16 @@ import Button from 'components/button';
 import { noop } from 'helper/noop';
 
 const ZERO = 0;
+const ONE = 1;
 
 const Switch = React.memo(({ data, switchIndex, onToggleSwitch = noop }) => {
   const renderSwitchs = data.map((item, itemIndex) => {
     const isActiveIndex = switchIndex === itemIndex;
-    const switchClass = isActiveIndex ? 'switch__item switch__item--active' : 'switch__item';
+    const activeClass = isActiveIndex ? 'switch__item switch__item--active ' : 'switch__item ';
     const ariaLabel = isActiveIndex ? `${item.label} mode is on` : `${item.label} mode is off`;
+    const firstClass = itemIndex === ZERO ? 'switch__item--first ': '';
+    const lastClass = itemIndex === data.length - ONE ? 'switch__item--last': '';
+    const switchClass = `${activeClass}${firstClass}${lastClass}`;
 
     return (
       <Button
