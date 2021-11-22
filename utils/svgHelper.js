d247f70c11f2a6bbf89c6e6e21cf7137b2055afb
Chris Varughese <cjamiev@icloud.com>
2021-11-21 20:15:26 -0600

    Adding class generator and sorting styles for svgHelper.

--- utils/svgHelper.js
+++ utils/svgHelper.js
@@ -12,6 +12,18 @@ const getTagAttribute = (line, attr) => {
   return attrString;
 };
 
+const getSortedStyleAttribute = (styleLine) => {
+  const sortedLine = styleLine
+    .replace('style="','')
+    .replace('"','')
+    .split(';')
+    .filter(prop => prop && !prop.includes('-inkscape-font-specification'))
+    .sort()
+    .join(';');
+
+  return `style="${sortedLine}"`;
+};
+
 const formatTagsToOneLine = (data) => {
   const splitLinesByOpeningTag = data
     .replace(/(\r|\t|\n)/gm, '')
@@ -67,9 +79,68 @@ const removeExtraneousInformation = (data) => {
   return updatedLines.join('\n');
 };
 
+const generateClassesFromStyles = (data) => {
+  const lines = data.split('\n');
+
+  const styleLines = lines
+    .map(currentLine => {
+      return getTagAttribute(currentLine, 'style=');
+    })
+    .filter(item => Boolean(item))
+    .map(currentLine => getSortedStyleAttribute(currentLine));
+
+  const uniqueStyleLines = styleLines.filter((i, position) => styleLines.indexOf(i) === position);
+
+  const generatedClasses = uniqueStyleLines
+    .map((styles,index) => {
+      const formattedStyles = styles
+        .replace('style="','')
+        .replace('"','')
+        .split(';')
+        .map(prop => {
+          return `\t${prop};\n`;
+        })
+        .join('');
+      const cssClass = `.name-${index} {\n${formattedStyles}}\n`;
+
+      return { cssClass, id: styles, className: `name-${index}` };
+    });
+
+  return generatedClasses;
+};
+
+const replaceStylesWithClass = (data, classes) => {
+  const lines = data.split('\n');
+
+  const updatedLines = lines.map((currentLine,index) => {
+    const styleLine = getTagAttribute(currentLine, 'style=');
+
+    if(!styleLine) {
+      return currentLine;
+    }
+
+    const styleId = getSortedStyleAttribute(styleLine);
+    const matched = classes.find(item => item.id === styleId);
+
+    if(!matched) {
+      throw new Error(`Match not found for: ${styleLine}`);
+    }
+
+    const updatedLine = currentLine
+      .replace(styleLine,`className="${matched.className}"`);
+
+    return updatedLine;
+  });
+
+  return updatedLines.join('\n');
+};
+
 export {
   getTagAttribute,
+  getSortedStyleAttribute,
   formatTagsToOneLine,
   formatTagsWithIndents,
-  removeExtraneousInformation
+  removeExtraneousInformation,
+  generateClassesFromStyles,
+  replaceStylesWithClass
 };
