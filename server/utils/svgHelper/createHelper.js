1ec56d59f1a646ee410400f1fc1d0e1ac40f08e0
Chris Varughese <cjamiev@icloud.com>
2021-12-13 23:54:38 -0600

    Moving parsing and writing svg component to it's own file

--- server/utils/svgHelper/createHelper.js
+++ server/utils/svgHelper/createHelper.js
@@ -1,269 +1,47 @@
-const { capitalizeFirstLetter, toCamelCaseFromDashCase } = require('../stringHelper');
-const { subcomponentTemplate, componentTemplate, importTemplate, indexTemplate, svgMapperTemplate, singleTemplate } = require('./templates');
-const { getAttributeList } = require('./attributeHelper');
+const { loadFile, writeToFile } = require('../file');
+const { formatTagsToOneLine, removeExtraneousInformation } = require('./cleanupHelper');
+const { generateClassesFromStyles, replaceStylesWithClass } = require('./classHelper');
+const { sortAttributes } = require('./attributeHelper');
+const { createSingleComponent, createReactComponents } = require('./componentHelper');
 
-const ZERO = 0;
-const ONE = 1;
+const parseSVGFile = (svgPath) => {
+  const svgFile = loadFile(svgPath);
+  const formattedSvgFile = formatTagsToOneLine(svgFile);
+  const { svgTagAttributes, cleanSvgFile } = removeExtraneousInformation(formattedSvgFile);
+  const classes = generateClassesFromStyles(cleanSvgFile);
+  const svgWithClasses = replaceStylesWithClass(cleanSvgFile, classes);
+  const data = sortAttributes(svgWithClasses);
 
-// Remove functiion
-const formatTagsWithIndents = (data) => {
-  const lines = data.split('\n');
-  let indentCount = 0;
-
-  const updatedLines = lines.map(currentLine => {
-    if (currentLine.includes('</')) {
-      indentCount--;
-    }
-    const spaces = Array.from({ length: indentCount }, i => '  ').join('');
-    if (!currentLine.includes('</') && !currentLine.includes('/>')) {
-      indentCount++;
-    }
-
-    return spaces + currentLine.trim();
-  });
-
-  return updatedLines.join('\n');
+  return { classes, svgTagAttributes, data };
 };
 
-const handleRemoveMode = (line, counter) => {
-  if(counter === ZERO && line.includes('</')) {
-    return { flag: false, counter: 0 };
-  } else if(line.includes('</')) {
-    return { flag: true, counter: counter - ONE };
-  } else if(line.includes('<') && line.includes('/>')) {
-    return { flag: true, counter };
-  } else if(line.includes('<') && line.includes('>')) {
-    return { flag: true, counter: counter + ONE };
-  }
-};
+const writeComponentsToFile = ({ classes, svgTagAttributes, data }, targetPath, isSingleComponent = false) => {
+  const generatedContent = isSingleComponent
+    ? createSingleComponent(svgTagAttributes, data)
+    : createReactComponents(svgTagAttributes, data);
 
-const removeSpecifiedSvg = (section) => {
-  let isRemoving = false;
-  let count = 0;
-
-  return section
-    .split('\n')
-    .filter(currentLine => !(currentLine.includes('data-testid="remove-') && currentLine.includes('/>')))
-    .map(currentLine => {
-      if(isRemoving) {
-        const { flag, counter } = handleRemoveMode(currentLine, count);
-
-        isRemoving = flag;
-        count = counter;
-
-        return '';
-      } else if (!isRemoving && currentLine.includes('data-testid="remove-')) {
-        isRemoving = true;
-
-        return '';
-      }
-
-      return currentLine;
-    })
-    .filter(Boolean)
-    .join('\n');
-};
-
-const handleConditionMode = (line, counter) => {
-  if(counter === ZERO && line.includes('</')) {
-    return { flag: false, counter: 0 };
-  } else if(line.includes('</')) {
-    return { flag: true, counter: counter - ONE };
-  } else if(line.includes('<') && line.includes('/>')) {
-    return { flag: true, counter };
-  } else if(line.includes('<') && line.includes('>')) {
-    return { flag: true, counter: counter + ONE };
-  }
-};
-
-const addCondition = (section) => {
-  let isAddingCondition = false;
-  let count = 0;
-  const conditions = [];
-  const currentCondition = {
-    name: '',
-    value: []
-  };
-
-  const updatedSvgObj = section
-    .split('\n')
-    .map(currentLine => {
-      if (!isAddingCondition && currentLine.includes('data-testid="condition-')) {
-        currentCondition.name && conditions.push({
-          name: currentCondition.name,
-          value: currentCondition.value.join('\n')
-        });
-        isAddingCondition = true;
-
-        const dashCaseName = getAttributeList(currentLine, 'data-testid="condition-')[ZERO]
-          .replace('data-testid="condition-','')
-          .replace('"','');
-        const name = capitalizeFirstLetter(toCamelCaseFromDashCase(dashCaseName));
-        currentCondition.name = name;
-        currentCondition.value = [currentLine.replace('data-testid="condition-','data-testid="')];
-
-        return '';
-      } else if(isAddingCondition) {
-        const { flag, counter } = handleConditionMode(currentLine, count);
-
-        isAddingCondition = flag;
-        count = counter;
-        currentCondition.value.push(currentLine);
-
-        return '';
-      }
-
-      return currentLine;
-    })
-    .filter(Boolean)
-    .join('\n');
-
-  currentCondition.name && conditions.push({
-    name: currentCondition.name,
-    value: currentCondition.value.join('\n')
-  });
-
-  return { conditions, updatedSvgObj};
-};
-
-const addConditionsToSpecifiedSvg = (section) => {
-  let conditions = [];
-
-  let updatedSvgObj = section
-    .split('\n')
-    .map(currentLine => {
-      if (currentLine.includes('data-testid="condition-') && currentLine.includes('/>')) {
-        const dashCaseName = getAttributeList(currentLine, 'data-testid="condition-')[ZERO]
-          .replace('data-testid="condition-','')
-          .replace('"','');
-        const name = capitalizeFirstLetter(toCamelCaseFromDashCase(dashCaseName));
-        conditions.push({
-          name,
-          value: currentLine.replace('data-testid="condition-','data-testid="')
-        });
-
-        return `<${name}SVG DELETE />`;
-      }
-
-      return currentLine;
-    })
-    .filter(Boolean)
-    .join('\n');
-
-  while(updatedSvgObj.includes('data-testid="condition-')) {
-    const result = addCondition(updatedSvgObj);
-    conditions = conditions.concat(result.conditions);
-
-    updatedSvgObj = result.updatedSvgObj;
-  }
-
-  // Error correction not sure what is the source of this problem
-  updatedSvgObj = updatedSvgObj.split('\n').filter(item => !item.includes('DELETE')).join('\n');
-
-  return { updatedSvgObj, conditions };
-};
-
-const createSingleComponent = (svgTagAttributes, data) => {
-  const svgMapperContent = singleTemplate
-    .replace('{{svgTagAttributes}}', svgTagAttributes)
-    .replace('{{jsxContent}}', removeSpecifiedSvg(data));
-
-  return { svgmapperjs: svgMapperContent };
-};
-
-const createReactComponents = (svgTagAttributes, data) => {
-  const lines = data.split('\n');
-
-  const parsedSVGObjects = lines
-    .map(currentLine => {
-      if(currentLine.includes('data-testid="component-')) {
-        return `MARK${currentLine}`;
-      } else {
-        return currentLine;
-      }
-    })
-    .join('\n')
-    .split('MARK')
-    .splice(ONE)
-    .map(currentSegment => {
-      const dashCaseName = getAttributeList(currentSegment, 'data-testid="component-')[ZERO]
-        .replace('data-testid="component-','')
-        .replace('"','');
-
-      const name = capitalizeFirstLetter(toCamelCaseFromDashCase(dashCaseName));
-      const svgObj = currentSegment
-        .replace('data-testid="component-','data-testid="')
-        .split('\n')
-        .filter(item => Boolean(item))
-        .map(line => `      ${line}`)
-        .join('\n');
-
-      return { name, svgObj };
-    })
-    .sort((itemA, itemB) => {
-      if(itemA.name > itemB.name) {
-        return ONE;
-      } else {
-        return -ONE;
-      }
-    })
-    .map(entry => {
-      const filteredSvgObject = removeSpecifiedSvg(entry.svgObj);
-
-      return { ...entry, svgObj: filteredSvgObject};
-    })
-    .map(entry => {
-      const { updatedSvgObj, conditions } = addConditionsToSpecifiedSvg(entry.svgObj);
-
-      return { ...entry, svgObj: updatedSvgObj, conditions };
-    })
-    .map(entry => {
-
-      const jsonDataTemplate = entry.conditions ? entry.conditions
-        .map(item => {
-          return `{ component:'${item.name}', transform: 'translate(0,0)' }`;
-        })
-        .join(',') : '';
-      const conditionList = entry.conditions ? entry.conditions.map(item => {
-        return subcomponentTemplate
-          .replace('{{name}}', item.name + 'SVG')
-          .replace('{{subcomponentSVG}}', item.value);
-      }).join('\n') : '';
-
-      const component = componentTemplate
-        .replace(/{{name}}/g, `${entry.name}SVG`)
-        .replace('{{subcomponents}}', conditionList)
-        .replace('{{svgObj}}', entry.svgObj);
-
-      return { componentInfo: { name: entry.name, subcomponentNames: entry.conditions }, component, jsonDataTemplate };
+  const cssClasses = classes.map(item => item.cssClass).join('\n');
+  writeToFile(`${targetPath}svg.css`, cssClasses);
+  if(!isSingleComponent) {
+    generatedContent.svgObjects.forEach(entry => {
+      writeToFile(`${targetPath}${entry.componentInfo.name}SVG.js`, entry.component);
     });
-
-  const importContent = parsedSVGObjects
-    .map(entry => importTemplate
-      .replace('{{imports}}', `${entry.componentInfo.name}SVG,\n  ${entry.componentInfo.subcomponentNames.map(item => { return `  ${item.name}SVG`; }).join(',\n')}`)
-      .replace('{{name}}', `${entry.componentInfo.name}SVG`)
-    )
-    .join('\n');
-  const svgMapContent = parsedSVGObjects.map(entry => {
-    const componentName = `'${entry.componentInfo.name}': ${entry.componentInfo.name}SVG`;
-    const subComponentNames = entry.componentInfo.subcomponentNames.map(item => {
-      return `'${item.name}': ${item.name}SVG`;
-    }).join(',\n');
-
-    return `${componentName},${subComponentNames}`;
-  });
-  const indexContent = indexTemplate
-    .replace('{{imports}}', importContent)
-    .replace('{{svgMap}}', `const svgMap = {\n${svgMapContent}\n};`);
-
-  const svgMapperContent = svgMapperTemplate
-    .replace('{{svgTagAttributes}}', svgTagAttributes);
-
-  return { indexjs: indexContent, svgObjects: parsedSVGObjects, svgmapperjs: svgMapperContent };
+    writeToFile(`${targetPath}index.js`, generatedContent.indexjs);
+    const svgJsonDataTemplate = generatedContent.svgObjects
+      .map(entry => {
+        return `\n  { component: '${entry.componentInfo.name}', transform: 'translate(0,0)', subcomponents: [${entry.jsonDataTemplate}] }`;
+      })
+      .join(',');
+    const jsonDataTemplate = `const testData = [${svgJsonDataTemplate}\n];`;
+    writeToFile(`${targetPath}SvgMapper.js`, generatedContent.svgmapperjs.replace('{{jsonDataTemplate}}', jsonDataTemplate));
+  } else {
+    writeToFile(`${targetPath}SvgMapper.js`, generatedContent.svgmapperjs.replace('{{jsonDataTemplate}}', ''));
+  }
 };
 
-module.exports = {
-  formatTagsWithIndents,
-  createSingleComponent,
-  createReactComponents
+const createSvgComponents = (svgPath, targetPath, isSingleComponent) => {
+  const { classes, svgTagAttributes, data } = parseSVGFile(svgPath);
+  writeComponentsToFile({ classes, svgTagAttributes, data }, targetPath, isSingleComponent);
 };
+
+module.exports = { createSvgComponents };
--- server/utils/svgHelper/index.js
+++ server/utils/svgHelper/index.js
@@ -1,23 +1,3 @@
-const {
-  formatTagsToOneLine,
-  removeExtraneousInformation
-} = require('./cleanupHelper');
-const {
-  generateClassesFromStyles,
-  replaceStylesWithClass
-} = require('./classHelper');
-const { sortAttributes } = require('./attributeHelper');
-const {
-  createSingleComponent,
-  createReactComponents
-} = require('./createHelper');
+const { createSvgComponents } = require('./createHelper');
 
-module.exports = {
-  formatTagsToOneLine,
-  removeExtraneousInformation,
-  generateClassesFromStyles,
-  replaceStylesWithClass,
-  sortAttributes,
-  createSingleComponent,
-  createReactComponents
-};
+module.exports = { createSvgComponents };
