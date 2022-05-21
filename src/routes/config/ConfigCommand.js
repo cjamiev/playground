0f84a9afdec46e466a4aa8daca17fe1e7097ef8a
Chris Varughese <cjamiev@icloud.com>
2022-05-20 19:53:07 -0500

    Adding create form for command config

--- src/routes/config/ConfigCommand.js
+++ src/routes/config/ConfigCommand.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import Button from 'components/button';
+import Text from 'components/form/Text';
+import { TrashSVG } from 'components/icons/TrashSVG';
 import { updateConfig } from './configActions';
 import { Table } from './ConfigTable';
-import { SCTableCell } from './styles';
+import { SCTableCell, SCTableCellIcon, SCTableCellSvg, SCCreateFormFieldSet } from './styles';
 
-const commandTableHeaders = [{ label: 'Description' }, { label: 'File' }];
+const commandTableHeaders = [{ label: 'Description' }, { label: 'File' }, { label: 'Delete' }];
 
 const ConfigCommand = ({ configCommands, onChange }) => {
   const dispatch = useDispatch();
+  const [newCommand, setNewCommand] = useState({ label: '', value: '' });
   const [commandConfiguration, setCommandConfiguration] = useState([]);
 
   useEffect(() => {
@@ -24,6 +28,21 @@ const ConfigCommand = ({ configCommands, onChange }) => {
           <SCTableCell>
             <span>{commandname.value}</span>
           </SCTableCell>
+          <SCTableCellIcon>
+            <SCTableCellSvg
+              aria-label="Delete"
+              width="45"
+              height="53"
+              viewBox="0 0 53 53"
+              onClick={() => {
+                const updatedCommand = commandConfiguration.filter((item) => item.value !== commandname.value);
+
+                onChange(updatedCommand);
+              }}
+            >
+              <TrashSVG transform={'scale(0.6) translate(35,-2)'} />
+            </SCTableCellSvg>
+          </SCTableCellIcon>
         </tr>
       );
     });
@@ -34,6 +53,34 @@ const ConfigCommand = ({ configCommands, onChange }) => {
       <h2> Commands </h2>
       <div>
         <Table headers={commandTableHeaders} body={renderCommandCells(global.commands)} />
+        <form>
+          <SCCreateFormFieldSet>
+            <legend> Create New Command </legend>
+            <Text
+              placeholder="Description"
+              selected={newCommand.label}
+              onChange={({ selected }) => {
+                setNewCommand({ label: selected, value: newCommand.value, id: commandConfiguration.length });
+              }}
+            />
+            <Text
+              placeholder="File"
+              selected={newCommand.value}
+              onChange={({ selected }) => {
+                setNewCommand({ label: newCommand.label, value: selected, id: commandConfiguration.length });
+              }}
+            />
+            <Button
+              classColor="primary"
+              label="Submit"
+              onClick={() => {
+                const updatedCommands = [newCommand].concat(commandConfiguration);
+
+                onChange(updatedCommands);
+              }}
+            />
+          </SCCreateFormFieldSet>
+        </form>
       </div>
     </div>
   );
