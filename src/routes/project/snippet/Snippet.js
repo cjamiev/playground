f6e6ee6e32d58ee28743ecd5e01009a3d23c1078
Chris Varughese <cjamiev@icloud.com>
2022-05-21 23:07:16 -0500

    redesigning Snippet

--- src/routes/project/snippet/Snippet.js
+++ src/routes/project/snippet/Snippet.js
@@ -6,15 +6,16 @@ import { ICON_TYPES } from 'constants/icon';
 import Button, { IconButton } from 'components/button';
 import Text from 'components/form/Text';
 import TextArea from 'components/form/TextArea';
+import { SCCreateFormFieldSet, SCLoadHeader, SCButtonGroup } from './styles';
 
 const Snippet = () => {
   const dispatch = useDispatch();
   const [name, setName] = useState('');
   const [content, setContent] = useState('');
-  const { snippets, snippetFile } = useSelector(state => state.project);
+  const { snippets, snippetFile } = useSelector((state) => state.project);
 
   useEffect(() => {
-    if(!snippets.length) {
+    if (!snippets.length) {
       dispatch(loadSnippetDirectory());
     }
   }, [dispatch, snippets]);
@@ -23,52 +24,61 @@ const Snippet = () => {
     setContent(snippetFile);
   }, [snippetFile]);
 
-  const fileButtons = snippets.map(label => {
-    return <Button
-      key={label}
-      label={label}
-      className="load-file__btns"
-      onClick={() => {
-        dispatch(loadSnippet(label));
-      }}
-    />;
+  const fileButtons = snippets.map((label) => {
+    return (
+      <Button
+        key={label}
+        label={label}
+        className="load-file__btns"
+        primary
+        onClick={() => {
+          dispatch(loadSnippet(label));
+        }}
+      />
+    );
   });
 
   return (
-    <div className="flex--horizontal container--center">
-      <div className="flex--vertical flex--one">
-        <h2 className="horizontal-center">Load File</h2>
-        {fileButtons}
+    <div className="flex--horizontal">
+      <div>
+        <form>
+          <SCCreateFormFieldSet>
+            <legend> Create Snippet </legend>
+            <Text
+              placeholder="Snippet Name"
+              selected={name}
+              onChange={({ selected }) => {
+                setName(selected);
+              }}
+            />
+            <Button
+              label="Create New"
+              classColor="primary"
+              onClick={() => {
+                if (name && content) {
+                  dispatch(createSnippet(name, content));
+                }
+              }}
+            />
+          </SCCreateFormFieldSet>
+        </form>
+        <SCLoadHeader>
+          <h2>Load File</h2>
+          <IconButton
+            type={ICON_TYPES.COPY}
+            onClick={() => {
+              copyToClipboard(snippetFile);
+            }}
+          />
+        </SCLoadHeader>
+        <SCButtonGroup>{fileButtons}</SCButtonGroup>
       </div>
-      <IconButton
-        type={ICON_TYPES.COPY}
-        onClick={() => {
-          copyToClipboard(snippetFile);
-        }}
-      />
       <div className="project__file-area flex--three">
         <TextArea
           ariaLabel="Enter Content"
           selected={content}
-          onChange={({ selected }) => { setContent(selected); }}
-        />
-      </div>
-      <div className="flex--vertical flex--one">
-        <h2>Create Snippet</h2>
-        <Text
-          placeholder="Snippet Name"
-          selected={name}
           onChange={({ selected }) => {
-            setName(selected);
-          }}
-        />
-        <Button
-          label="Create New"
-          classColor="secondary"
-          onClick={() => {
-            if(name && content) {
-              dispatch(createSnippet(name, content));
-            }
+            setContent(selected);
           }}
         />
       </div>
---
+++ src/routes/project/snippet/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const SCCreateFormFieldSet = styled.fieldset`
+  width: 350px;
+  padding: 10px;
+  border-width: 2px;
+  border-style: groove;
+  border-color: threedface;
+  border-image: initial;
+
+  div {
+    margin-bottom: 10px;
+    margin-left: 6px;
+  }
+
+  button {
+    margin-left: 25%;
+  }
+`;
+
+export const SCLoadHeader = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+
+  h2 {
+    margin-top: 10px;
+  }
+`;
+
+export const SCButtonGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  margin-left: -10px;
+`;
