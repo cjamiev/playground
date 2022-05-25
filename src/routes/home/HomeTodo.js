033831678aeaf4bfca3e0e0f12b75c631eb381a3
Chris Varughese <cjamiev@icloud.com>
2022-05-25 10:00:13 -0500

    Redesigning the Todo list

--- src/routes/home/HomeTodo.js
+++ src/routes/home/HomeTodo.js
@@ -5,7 +5,7 @@ import { getEllipsisForLongText } from 'stringHelper';
 import { ArrowSVG } from 'components/icons/ArrowSVG';
 import { TrashSVG } from 'components/icons/TrashSVG';
 import { PenSVG } from 'components/icons/PenSVG';
-import { SCHomeCardWrapper, SCHomeFooter } from './styles';
+import { SCTodoWrapper, SCTodoTitleWrapper, SCTodoList, SCHomeFooter } from './styles';
 
 const ZERO = 0;
 const MAX_LENGTH = 18;
@@ -32,76 +32,68 @@ const HomeTodo = ({ tasks, onChange, onEditTask }) => {
   };
 
   return (
-    <div className="flex--horizontal">
+    <div>
       {tasks.length > ZERO ? (
         tasks.map(({ id, text, notes, urls }) => (
-          <SCHomeCardWrapper key={id} isLarge>
-            <Card
-              title={text}
-              body={
-                <div className="flex--vertical">
-                  {notes.map((note) => (
-                    <span key={note} className="home__task-note">
-                      {note}
-                    </span>
-                  ))}
-                  {urls.map((url) => (
-                    <a key={url} className="link home__task-link" href={url} target="_blank">
-                      <label className="home__task-link-label">{getEllipsisForLongText(url, MAX_LENGTH)}</label>
-                    </a>
-                  ))}
-                </div>
-              }
-              footer={
-                <SCHomeFooter>
-                  <svg
-                    aria-label="Edit"
-                    width="45"
-                    height="53"
-                    viewBox="0 0 53 53"
-                    onClick={() => {
-                      onEditTask({ id, text, notes, urls });
-                    }}
-                  >
-                    <PenSVG transform={'translate(0,0)'} />
-                  </svg>
-                  <svg
-                    aria-label="Delete"
-                    width="45"
-                    height="53"
-                    viewBox="0 0 53 53"
-                    onClick={() => {
-                      removeItem(id);
-                    }}
-                  >
-                    <TrashSVG transform={'translate(0,0)'} />
-                  </svg>
-                  <svg
-                    aria-label="Move Up"
-                    width="45"
-                    height="53"
-                    viewBox="0 0 53 53"
-                    onClick={() => {
-                      moveItemUp(id);
-                    }}
-                  >
-                    <ArrowSVG transform={'translate(0,0)'} conditions={{ orientation: 'UP' }} />
-                  </svg>
-                  <svg
-                    aria-label="Move Down"
-                    width="45"
-                    height="53"
-                    viewBox="0 0 53 53"
-                    onClick={() => {
-                      moveItemDown(id);
-                    }}
-                  >
-                    <ArrowSVG transform={'translate(0,0)'} conditions={{ orientation: 'DOWN' }} />
-                  </svg>
-                </SCHomeFooter>
-              }
-            />
-          </SCHomeCardWrapper>
+          <SCTodoWrapper key={id}>
+            <SCTodoTitleWrapper>
+              <h2>{text}</h2>
+              <svg
+                aria-label="Delete"
+                width="27"
+                height="27"
+                viewBox="0 0 53 53"
+                onClick={() => {
+                  removeItem(id);
+                }}
+              >
+                <TrashSVG />
+              </svg>
+              <svg
+                aria-label="Move Up"
+                width="27"
+                height="27"
+                viewBox="0 0 53 53"
+                onClick={() => {
+                  moveItemUp(id);
+                }}
+              >
+                <ArrowSVG conditions={{ orientation: 'UP' }} />
+              </svg>
+              <svg
+                aria-label="Move Down"
+                width="27"
+                height="27"
+                viewBox="0 0 53 53"
+                onClick={() => {
+                  moveItemDown(id);
+                }}
+              >
+                <ArrowSVG conditions={{ orientation: 'DOWN' }} />
+              </svg>
+              <svg
+                aria-label="Edit"
+                width="27"
+                height="27"
+                viewBox="0 0 53 53"
+                onClick={() => {
+                  onEditTask({ id, text, notes, urls });
+                }}
+              >
+                <PenSVG />
+              </svg>
+            </SCTodoTitleWrapper>
+            <SCTodoList>
+              {notes.map((note) => (
+                <li key={note}>{note}</li>
+              ))}
+              {urls.map((url) => (
+                <a key={url} href={url} target="_blank">
+                  <label>{getEllipsisForLongText(url, MAX_LENGTH)}</label>
+                </a>
+              ))}
+            </SCTodoList>
+          </SCTodoWrapper>
         ))
       ) : (
         <p> No tasks to display </p>
--- src/routes/home/styles.js
+++ src/routes/home/styles.js
@@ -28,3 +28,46 @@ export const SCHomeFooter = styled.div`
     transform: scale(1.1);
   }
 `;
+
+export const SCTodoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  min-height: 200px;
+  padding-top: 10px;
+  padding-left: 10px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+
+  box-shadow: 0px 4px 4px 1px hsl(0, 0%, 80%);
+`;
+
+export const SCTodoTitleWrapper = styled.div`
+  display: flex;
+  position: relative;
+
+  h2 {
+    font-size: 24px;
+    text-decoration: underline;
+    min-width: 18ch;
+  }
+
+  svg {
+    position: relative;
+    margin: 0;
+    padding: 0;
+    cursor: pointer;
+  }
+  svg:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const SCTodoList = styled.ul`
+  display: flex;
+  flex-direction: column;
+
+  li {
+    font-size: 20px;
+  }
+`;
