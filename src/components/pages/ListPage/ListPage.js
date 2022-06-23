d008e9071ea42d6926b2c5c9c355b893d071399c
Chris Varughese <cjamiev@icloud.com>
2022-06-22 21:32:50 -0500

    Adding in list

--- src/components/pages/ListPage/ListPage.js
+++ src/components/pages/ListPage/ListPage.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import Page from 'components/layout/Page';
+import CollapsedList from 'components/atoms/CollapsedList';
+
+const collapseListData = [
+  {
+    shortLabel: '1',
+    label: 'Item1',
+    handleClick: () => {
+      console.log('Item1');
+    },
+    isActive: false
+  },
+  {
+    shortLabel: '2',
+    label: 'Item2',
+    handleClick: () => {
+      console.log('Item2');
+    },
+    isActive: true
+  },
+  {
+    shortLabel: '3',
+    label: 'Item3',
+    handleClick: () => {
+      console.log('Item3');
+    },
+    isActive: false
+  }
+];
 
 const ListPage = () => {
-  return <Page>List test</Page>;
+  return (
+    <Page>
+      <CollapsedList data={collapseListData} />
+    </Page>
+  );
 };
 
 export default ListPage;
